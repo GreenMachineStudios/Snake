08bd1c4343c230252dbd2306de92e5e377dda539
GreenMachineStudios <seamonkey777@proton.me>
2023-12-04 10:38:32 -0800

    Add project files.

---
+++ Snake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] DirectionChars = ['^', 'v', '<', '>',];
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+
+            string prompt = $"Press any key to start...";
+            Console.Write(prompt);
+            Console.ReadKey(true);
+            Console.Clear();
+
+        }
+
+    }
+
+}
